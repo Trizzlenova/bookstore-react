430a5e5996daffdc4964514f2dfe725c34208bc6
bcorman <benjamincorman@gmail.com>
2018-06-26 14:19:09 -0700

    fixed header. pushed to heroku

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,12 @@ import React, {Component} from 'react';
 import {Navbar, NavItem} from 'react-materialize'
 import {Link} from 'react-router-dom';
 
+
 class Header extends Component{
   render(){
     return (
-      <Navbar className="#2979ff blue accent-3" brand='logo' right>
+      <Navbar className="#2979ff blue accent-3" left>
+        <NavItem className="left"><img id="bat" src={'images/bat.png'}/></NavItem>
         <NavItem><Link to={'/'}>Home</Link></NavItem>
         <NavItem><Link to={'/books'}>Books</Link></NavItem>
       </Navbar>
